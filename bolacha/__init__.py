6dea3cd89b6eb255e5c9d5068fcf76f871498a5b
Gabriel Falcão <gabriel@nacaolivre.org>
2009-08-06 22:12:37 -0300

    Minor release 0.1.1-Maisena

--- bolacha/__init__.py
+++ bolacha/__init__.py
@@ -23,7 +23,7 @@ from bolacha.multipart import encode_multipart
 from bolacha.multipart import urlencode
 from bolacha.multipart import is_file
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 __release__ = 'Maisena'
 
 HTTP_METHODS = (
